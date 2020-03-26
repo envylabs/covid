440fbf63b62b3123effd580c5094876c02e4128f
Mark Kendall <mark@envylabs.com>
2020-03-26 11:13:02 -0400

    Update production database config for gigalixir

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -12,9 +12,10 @@ database_url =
     """
 
 config :rona, Rona.Repo,
-  # ssl: true,
   url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  database: "",
+  ssl: true,
+  pool_size: 2
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||

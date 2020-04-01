4c2e8e3c039df5dbe924b01799bdbc4c37b9b7d1
Mark Kendall <mark@envylabs.com>
2020-04-01 11:31:52 -0400

    Change domain name

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :rona, RonaWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "rona.envylabs.com", port: 443],
+  url: [scheme: "https", host: "covid.envylabs.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 

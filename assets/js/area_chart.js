ada38e583b1925c43bc062c9306381fb5fd5c9fe
Mark Kendall <mark@envylabs.com>
2020-04-05 14:54:39 -0400

    Update chart colors

--- assets/js/area_chart.js
+++ assets/js/area_chart.js
@@ -3,6 +3,11 @@ const height = 100;
 
 const margin = ({top: 10, right: 10, bottom: 30, left: 40});
 
+const colors = [
+	["rgb(136, 68, 157)", "rgb(204, 221, 236)"],
+	["rgb(33, 139, 72)", "rgb(193, 232, 224)"]
+];
+
 const xAxis = (g, obj) => {
   g.attr("transform", `translate(0,${height - margin.bottom})`)
     .call(d3.axisBottom(obj.x).ticks(2).tickSizeOuter(0));
@@ -16,11 +21,11 @@ const yAxis = (g, obj) => {
 
 const prepareChart = (obj) => {
   const data = JSON.parse(obj.el.dataset.chartdata).map(d => ({t: new Date(d.t), c: d.c, d: d.d}));
-  obj.data = d3.stack().keys(["c", "d"])(data);
+  obj.data = d3.stack().keys(["d", "c"])(data);
   obj.max = obj.el.dataset.max;
 
   obj.color = d3.scaleOrdinal()
-    .range(d3.schemeCategory10);
+    .range(colors[obj.el.dataset.colorScheme]);
 
   obj.x = d3.scaleUtc()
     .domain(d3.extent(data, d => d.t))
